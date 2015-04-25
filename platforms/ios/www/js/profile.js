701a75221da1c4d7d759b5bad3e5f850742a70a6
Eric Chee <bboyairwreck@gmail.com>
2015-04-25 13:29:28 -0700

    Renamed html,js,css files to make more sense.

--- platforms/ios/www/js/profile.js
+++ platforms/ios/www/js/profile.js
@@ -1,38 +1,66 @@
-/**
- * Created by wendykung on 4/25/15.
- */
 $(document).ready(function() {
-    // grab local variable - personID to get companionID (22)
-    //$id = localStorage.getItem("user_personID");
 
-    $.ajax({
-        crossDomain: true,
-        contentType: "application/json; charset=utf-8",
-        url: "http://ericchee.com/neverforgotten/relatedCompanions.php",
-        data: { n: 22 },
-        dataType : "json",
-        success : ajaxSuccess,
-        error : ajaxError
+    $("#editButton").click(function(){
+        $name = $("#main").text();
+        $("#editMain").text($name);
+        $("input[name='name']").prop('value', $name);
+        $dob = $("#birthdate").text();
+        $("input[name='dob']").prop('value', $dob);
+        $address = $("#address").text();
+        $("input[name='address']").prop('value', $address);
+        $citystate = $("#citystate").text();
+        $("input[name='citystate']").prop('value', $citystate);
+        $phone = $("#phone").text();
+        $("input[name='phone']").prop('value', $phone);
+        $emergency = $("#emergency").text();
+        $("input[name='emergency']").prop('value', $emergency);
+        $doctor = $("#doctor").text();
+        $("input[name='doctor']").prop('value', $doctor);
+        $hospital = $("#hospital").text();
+        $("input[name='hospital']").prop('value', $hospital);
+        $blood = $("#blood").text();
+        $("input[name='blood']").prop('value', $blood);
+        $allergies = "";
+        $("#allergies li").each(function(index) {
+            $allergies = $allergies + $(this).text() + " ";
+        });
+        alert($allergies);
+        $("input[name='allergies']").prop('value', $allergies);
+        $meds = "";
+        $("#meds li").each(function(index) {
+            $meds = $meds + $(this).text() + " ";
+        });
+        alert($meds);
+        $("input[name='meds']").prop('value', $meds);
+        $insurance = $("#insurance").text();
+        $("input[name='insurance']").prop('value', $insurance);
+    });
+
+    // update profile information
+    $("#submitProfile").click(function() {
+        $setName = $("input[name='name']").prop('value');
+        $("#main").text($setName);
+        $setDob = $("input[name='dob']").prop('value');
+        $("#birthdate").text($setDob);
+        $setAddress = $("input[name='address']").prop('value');
+        $("#address").text($setAddress);
+        $setCityState = $("input[name='citystate']").prop('value');
+        $("#citystate").text($setCityState);
+        $setPhone = $("input[name='phone']").prop('value');
+        $("#phone").text($setPhone);
+        $setEmergency = $("input[name='emergency']").prop('value');
+        $("#emergency").text($setEmergency);
+        $setDoctor = $("input[name='doctor']").prop('value');
+        $("#doctor").text($setDoctor);
+        $setHospital = $("input[name='hospital']").prop('value');
+        $("#hospital").text($setHospital);
+        $setBlood = $("input[name='blood']").prop('value');
+        $("#blood").text($setBlood);
+        $setAllergies = $("input[name='allergies']").prop('value');
+        $("#allergies").text($setAllergies);
+        $setMeds = $("input[name='meds']").prop('value');
+        $("#meds").text($setMeds);
+        $setInsurance = $("input[name='insurance']").prop('value');
+        $("#insurance").text($setInsurance);
     });
 });
-
-function ajaxSuccess(data) {
-    for (var i = 0; i < data.length; i++) {
-        var $newProfile = $("#profTemp li.profileWrap").clone();
-        var $name = $newProfile.find("div.profName");
-        var $patientName = data[i]["PatientFName"] + " " + data[i]["PatientLName"];
-        var $personID = data[i]["PersonID"];
-        var $patientID = data[i]["PatientID"];
-        $name.text($patientName);
-        $newProfile.attr('data-personID', $personID);
-        $newProfile.attr('data-patientID', $patientID);
-        $("#profList").prepend($newProfile);
-    }
-}
-
-function ajaxError( xhr, status, errorThrown ) {
-    alert( "Sorry, there was an ajax problem!" );
-    console.log( "Error: " + errorThrown );
-    console.log( "Status: " + status );
-    console.dir( xhr );
-}
