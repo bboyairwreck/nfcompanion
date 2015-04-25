ea26b975fdcddf4a75190c138c50dc9df1b015e9
dwendle <dub_kung@hotmail.com>
2015-04-25 15:52:37 -0700

    update calendar page

---
+++ www/js/calendar.js
@@ -0,0 +1,23 @@
+/**
+ * Created by wendykung on 4/25/15.
+ */
+$(document).ready(function() {
+
+
+
+
+
+
+});
+
+
+
+
+
+var dateObj = new Date("2015-04-24 9:00:00");
+
+dateObj.getMonth();
+
+
+
+
